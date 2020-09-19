bd385712f0acb8651f4b64a104b244773596b901
unknown <kyoung-min92@naver.com>
2020-09-19 23:25:03 +0900

    log data save file

--- android_bluno_demo_app-master/src/net/craigiebabe/android/bluetoothlegatt/LogActivity.java
+++ android_bluno_demo_app-master/src/net/craigiebabe/android/bluetoothlegatt/LogActivity.java
@@ -1,5 +1,6 @@
 package net.craigiebabe.android.bluetoothlegatt;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -7,9 +8,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,9 +28,18 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import static android.content.ContentValues.TAG;
 
 public class LogActivity extends Activity {
     public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
@@ -37,7 +51,74 @@ public class LogActivity extends Activity {
     private String mDeviceAddress;
 
     private BluetoothLeService mBluetoothLeService;
+    private ArrayList Log_Data[] = new ArrayList[3];
+    private String index_str[] = {"temp","humid","ph"};
 
+    private void saveFile(){
+        File saveFile = null;
+        if( Build.VERSION.SDK_INT < 29) saveFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/lolSaveFiles");
+        else saveFile = LogActivity.this.getExternalFilesDir("/lolSaveFiles");
+
+        if(!saveFile.exists())
+            saveFile.mkdir();
+        try {
+            BufferedWriter buf = new BufferedWriter(new FileWriter(saveFile+"/savedData.txt", false));
+            for(int j = 0 ; j < 3; j++) {
+                for (int i = 0; i < Log_Data[j].size(); i++) {
+                    if (i == 0) {
+                        buf.append(index_str[j] + ",");
+                        Log.e(TAG, index_str[j]);
+                    }
+                    buf.append(Double.toString((double) Log_Data[j].get(i)));
+                    if (i != Log_Data[j].size() - 1)
+                        buf.append(",");
+                }
+                buf.newLine();
+            }
+            buf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadFile(){
+        String readLine = null;
+        File saveFile = null;
+
+        if( Build.VERSION.SDK_INT < 29) saveFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/lolSaveFiles");
+        else saveFile = LogActivity.this.getExternalFilesDir("/lolSaveFiles");
+        if(saveFile == null)
+            saveFile.mkdir();
+        try {
+            BufferedReader buf = new BufferedReader(new FileReader(saveFile+"/savedData.txt"));
+            while ((readLine = buf.readLine()) != null) {
+                String[] Data = readLine.split(",");
+                if(Data.length > 0) {
+                    if (Data[0].compareTo(index_str[0]) == 0) {
+                        for (int i = 1; i < Data.length; i++) {
+                            Log_Data[0].add(Double.parseDouble(Data[i]));
+                            addEntry(Double.parseDouble(Data[i]), 0);
+                        }
+                    } else if (Data[0].compareTo(index_str[1]) == 0) {
+                        for (int i = 1; i < Data.length; i++) {
+                            Log_Data[1].add(Double.parseDouble(Data[i]));
+                            addEntry(Double.parseDouble(Data[i]), 1);
+                        }
+                    } else if (Data[0].compareTo(index_str[2]) == 0) {
+                        for (int i = 1; i < Data.length; i++) {
+                            Log_Data[2].add(Double.parseDouble(Data[i]));
+                            addEntry(Double.parseDouble(Data[i]), 2);
+                        }
+                    }
+                }
+            }
+            buf.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     private void addEntry(double num, int index) {
 
         LineData data = chart[index].getData();
@@ -150,9 +231,25 @@ public class LogActivity extends Activity {
         }
     }
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+        saveFile();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+        Log_Data[0] = new ArrayList();
+        Log_Data[1] = new ArrayList();
+        Log_Data[2] = new ArrayList();
 
         final Intent intent = getIntent();
         mDeviceAddress = intent.getStringExtra(EXTRAS_DEVICE_ADDRESS);
@@ -208,8 +305,8 @@ public class LogActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Random random = new Random();
-                int randomValue = random.nextInt(10);
-
+                double randomValue = random.nextDouble();
+                Log_Data[0].add(randomValue);
                 addEntry(randomValue,0);
             }
         });
@@ -218,8 +315,8 @@ public class LogActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Random random = new Random();
-                int randomValue = random.nextInt(10);
-
+                double randomValue = random.nextDouble();
+                Log_Data[1].add(randomValue);
                 addEntry(randomValue,1);
             }
         });
@@ -228,13 +325,15 @@ public class LogActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Random random = new Random();
-                int randomValue = random.nextInt(10);
-
+                double randomValue = random.nextDouble();
+                Log_Data[2].add(randomValue);
                 addEntry(randomValue,2);
             }
         });
 
         Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
         bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+
+        loadFile();
     }
 }
